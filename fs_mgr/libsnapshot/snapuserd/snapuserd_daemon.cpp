9d01dfab93066496f3873304d169cf9665613a3a
Daniel Zheng <zhengdaniel@google.com>
2025-05-28 21:41:27 +0000

    fix log comment
    match comment to the check if check
    
    Test: th
    Change-Id: I68f598f4a0127ff72dafbb042534682850c44bfb

--- fs_mgr/libsnapshot/snapuserd/snapuserd_daemon.cpp
+++ fs_mgr/libsnapshot/snapuserd/snapuserd_daemon.cpp
@@ -115,7 +115,7 @@ bool Daemon::StartServerForUserspaceSnapshots(int arg_start, int argc, char** ar
     for (int i = arg_start; i < argc; i++) {
         auto parts = android::base::Split(argv[i], ",");
         if (parts.size() != 4) {
-            LOG(ERROR) << "Malformed message, expected at least four sub-arguments.";
+            LOG(ERROR) << "Malformed message, expected four sub-arguments.";
             return false;
         }
         HandlerOptions options = {
