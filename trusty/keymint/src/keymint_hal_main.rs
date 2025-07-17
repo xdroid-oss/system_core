3080ca040f9c40122182eaefbd8657b35c28fdee
Alice Wang <aliceywang@google.com>
2025-07-17 13:18:52 +0000

    refactor: keymint: Use common code for RoT data transmission
    Bug: 404289721
    Test: atest VtsAidlKeyMintTargetTest
    FLAG: EXEMPT refactor
    Change-Id: I826436048e0e625e77fc31343ed8269bb51406b9

--- trusty/keymint/src/keymint_hal_main.rs
+++ trusty/keymint/src/keymint_hal_main.rs
@@ -122,27 +122,7 @@ fn inner_main() -> Result<(), HalServiceError> {
     let tipc_channel = Arc::new(Mutex::new(TipcChannel(connection)));
 
     #[cfg(feature = "nonsecure")]
-    {
-        // When the non-secure feature is enabled, retrieve root-of-trust information
-        // (with the exception of the verified boot key hash) from Android properties, and
-        // populate the TA with this information. On a real device, the bootloader should
-        // provide this data to the TA directly.
-        let boot_req = kmr_hal_nonsecure::get_boot_info();
-        info!("boot/HAL->TA: boot info is {:?}", boot_req);
-        kmr_hal::send_boot_info(tipc_channel.lock().unwrap().deref_mut(), boot_req)
-            .map_err(|e| HalServiceError(format!("Failed to send boot info: {:?}", e)))?;
-        // When the non-secure feature is enabled, also retrieve device ID information
-        // (except for IMEI/MEID values) from Android properties and populate the TA with
-        // this information. On a real device, a factory provisioning process would populate
-        // this information.
-        let attest_ids = kmr_hal_nonsecure::attestation_id_info();
-        if let Err(e) =
-            kmr_hal::send_attest_ids(tipc_channel.lock().unwrap().deref_mut(), attest_ids)
-        {
-            error!("Failed to send attestation ID info: {:?}", e);
-        }
-        info!("Successfully sent non-secure boot info and attestation IDs to the TA.");
-    }
+    kmr_hal_nonsecure::send_boot_info_and_attestation_id_info(&tipc_channel)?;
 
     register_binder_services(&tipc_channel, SERVICE_INSTANCE)?;
 
