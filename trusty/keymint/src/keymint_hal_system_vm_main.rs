4f5eb410406939e9cdfa6842add9193d9091170e
Alice Wang <aliceywang@google.com>
2025-07-18 07:30:06 +0000

    keymint_in_vm: Use common code for HAL setup logic
    Bug: 404289721
    FLAG: EXEMPT refactor
    Test: atest VtsAidlKeyMintTargetTest
    
    Change-Id: If5cca5ad2edc2f8d180e7bdf954d3c3d029aebdf

--- trusty/keymint/src/keymint_hal_system_vm_main.rs
+++ trusty/keymint/src/keymint_hal_system_vm_main.rs
@@ -19,7 +19,7 @@
 use android_keymint_trusty_commservice::aidl::android::keymint::trusty::commservice::ICommService::ICommService;
 use anyhow::{anyhow, bail, Context, Result};
 use binder::{self, AccessorProvider, ProcessState, Strong};
-use kmr_hal::{keymint, rpc, secureclock, send_hal_info, sharedsecret, SerializedChannel};
+use kmr_hal::{register_binder_services, send_hal_info, SerializedChannel};
 use log::{error, info, warn};
 use std::{
     ops::DerefMut,
@@ -28,11 +28,6 @@ use std::{
 };
 
 const SERVICE_INSTANCE: &str = "default";
-
-const KM_SERVICE_NAME: &str = "android.hardware.security.keymint.IKeyMintDevice";
-const RPC_SERVICE_NAME: &str = "android.hardware.security.keymint.IRemotelyProvisionedComponent";
-const SECURE_CLOCK_SERVICE_NAME: &str = "android.hardware.security.secureclock.ISecureClock";
-const SHARED_SECRET_SERVICE_NAME: &str = "android.hardware.security.sharedsecret.ISharedSecret";
 
 const ACCESSOR_SERVICE_NAME: &str = "android.os.IAccessor/ICommService/default";
 const INTERNAL_RPC_SERVICE_NAME: &str = "android.keymint.trusty.commservice.ICommService/default";
@@ -99,30 +94,9 @@ fn inner_main() -> Result<()> {
     let channel: HalChannel = CommServiceChannel { comm_service }.into();
 
     #[cfg(feature = "nonsecure")]
-    {
-        // When the non-secure feature is enabled, retrieve root-of-trust information
-        // (with the exception of the verified boot key hash) from Android properties, and
-        // populate the TA with this information. On a real device, the bootloader should
-        // provide this data to the TA directly.
-        let boot_req = kmr_hal_nonsecure::get_boot_info();
-        info!("boot/HAL->TA: boot info is {:?}", boot_req);
-        channel
-            .with(|c| kmr_hal::send_boot_info(c, boot_req).context("failed to send boot info"))?;
+    kmr_hal_nonsecure::send_boot_info_and_attestation_id_info(&channel.0)?;
 
-        // When the non-secure feature is enabled, also retrieve device ID information
-        // (except for IMEI/MEID values) from Android properties and populate the TA with
-        // this information. On a real device, a factory provisioning process would populate
-        // this information.
-        let attest_ids = kmr_hal_nonsecure::attestation_id_info();
-        if let Err(e) = channel.with(|c| {
-            kmr_hal::send_attest_ids(c, attest_ids).context("failed to send attestation ID")
-        }) {
-            error!("failed to send attestation ID info: {:?}", e);
-        }
-        info!("Successfully sent non-secure boot info and attestation IDs to the TA.");
-    }
-
-    register_keymint_services(&channel.0)?;
+    register_binder_services(&channel.0, SERVICE_INSTANCE)?;
 
     // Send the HAL service information to the TA
     channel.with(|c| send_hal_info(c).context("failed to populate HAL info"))?;
@@ -144,31 +118,4 @@ fn setup_logging_and_panic_hook() {
     panic::set_hook(Box::new(|panic_info| {
         error!("PANIC: {}", panic_info);
     }));
-}
-
-fn register_keymint_services(channel: &Arc<Mutex<CommServiceChannel>>) -> Result<()> {
-    /// Helper to register a single HAL service.
-    fn register_hal<F, T>(
-        base_name: &str,
-        channel: &Arc<Mutex<CommServiceChannel>>,
-        constructor: F,
-    ) -> Result<()>
-    where
-        F: FnOnce(Arc<Mutex<CommServiceChannel>>) -> Strong<T>,
-        T: binder::FromIBinder + ?Sized,
-    {
-        let service = constructor(channel.clone());
-        let full_name = format!("{}/{}", base_name, SERVICE_INSTANCE);
-        binder::add_service(&full_name, service.as_binder())
-            .with_context(|| format!("failed to add service {full_name}"))?;
-        info!("Registered Binder service {full_name}.");
-        Ok(())
-    }
-
-    register_hal(KM_SERVICE_NAME, channel, keymint::Device::new_as_binder)?;
-    register_hal(RPC_SERVICE_NAME, channel, rpc::Device::new_as_binder)?;
-    register_hal(SECURE_CLOCK_SERVICE_NAME, channel, secureclock::Device::new_as_binder)?;
-    register_hal(SHARED_SECRET_SERVICE_NAME, channel, sharedsecret::Device::new_as_binder)?;
-
-    Ok(())
 }
